68cf71c1d1a06783c12503f6cd2f53c09d61eab9
Mathis Holler <m.holler@orange.fr>
2025-02-18 11:09:43 +0100

    Refonte js Partie 1

---
+++ game.js
@@ -0,0 +1,29 @@
+import {culture_Quizz} from './questionsQuizz.js'; 
+ 
+let currentQuestionIndex = 0;
+// Sélection des éléments HTML
+const questions = document.getElementById('question-text');
+const options = document.getElementById('options-container');
+const nextButton = document.getElementById('next-button');
+
+// Fonction pour afficher une question basée sur l'index actuel
+function loadQuestion(index) {
+  // Vider le conteneur des options
+options.innerHTML = '';
+
+  // Récupérer la question actuelle
+  const currentQuestion = culture_Quizz.questions[index];
+
+  // Injecter la question dans le HTML
+  questions.innerText = currentQuestion.question;
+
+  // Injecter les options dans le HTML 
+  currentQuestion.options.forEach(option => {
+    const button = document.createElement('button');
+    button.innerText = option;
+    button.classList.add('options-button');
+    options.appendChild(button);
+  });
+}
+
+loadQuestion(currentQuestionIndex)
