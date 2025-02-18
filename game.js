cb2eee34dd82e7c61554f8d7e568d1b6e81d1bff
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-02-18 14:34:04 +0100

    boutton suivant none

--- game.js
+++ game.js
@@ -1,39 +1,42 @@
-import {culture_Quizz} from './questionsQuizz.js'; 
- 
+import { culture_Quizz } from './questionsQuizz.js';
+
 let currentQuestionIndex = 0;
 // Sélection des éléments HTML
 const questions = document.getElementById('question-text');
 const options = document.getElementById('options-container');
 const nextButton = document.getElementById('next-button');
+const replayButton = document.getElementById('replay-button');
 
 // Fonction pour afficher une question basée sur l'index actuel
 function loadQuestion(index) {
-  // Vider le conteneur des options
-options.innerHTML = '';
+    // Vider le conteneur des options
+    options.innerHTML = '';
 
-  // Récupérer la question actuelle
-  const currentQuestion = culture_Quizz.questions[index];
+    // Récupérer la question actuelle
+    const currentQuestion = culture_Quizz.questions[index];
 
-  // Injecter la question dans le HTML
-  questions.innerText = currentQuestion.question;
+    // Injecter la question dans le HTML
+    questions.innerText = currentQuestion.question;
 
-  // Injecter les options dans le HTML 
-  currentQuestion.options.forEach(option => {
-    const button = document.createElement('button');
-    button.innerText = option;
-    button.classList.add('options-button');
-    options.appendChild(button);
-  });
+    // Injecter les options dans le HTML 
+    currentQuestion.options.forEach(option => {
+        const button = document.createElement('button');
+        button.innerText = option;
+        button.classList.add('options-button');
+        options.appendChild(button);
+    });
 }
 
 loadQuestion(currentQuestionIndex)
 
-nextButton.addEventListener("click" , () => {
+nextButton.addEventListener("click", () => {
     if (currentQuestionIndex < culture_Quizz.questions.length - 1) {
-        currentQuestionIndex++; 
+        currentQuestionIndex++;
         loadQuestion(currentQuestionIndex)
-    } else  {
-        console.log("Fin du Quiz")
+    } else {
+        options.innerHTML = '';
+        questions.innerHTML = '';
+        nextButton.style.display = 'none';
     }
 
 }) 
